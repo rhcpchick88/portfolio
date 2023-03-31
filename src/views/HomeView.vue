8cf96b5304f0d486703503fc4c1494e6e969d8ba
samantha <langridge.se@gmail.com>
2023-03-30 20:11:09 -0600

    practice portfolio finished

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,22 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="intro">
+      <h1> Samantha Langridge</h1>
+      <h2> Jr. Full-Stack Web Developer</h2>
+    </div>
+    <div class="links">
+      <a href="http://github.com/rhcpchick88">GitHub link</a> | 
+      <a href="http://linkedin.com/in/samantha-langridge-588024aa">LinkedIn profile</a>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+  export default {
+    
+  }
 </script>
+
+<style lang="sass" scoped>
+
+</style>
